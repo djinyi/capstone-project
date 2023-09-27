ef930bb16dd79d3cdc15f4595f3f93278667cb01
Daisy Yi <daisyyi@rocketmail.com>
2023-09-26 21:08:54 -0700

    set up Chakraprovider at root

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { UserProvider } from "./user/UserContext";
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
+  <ChakraProvider>
   <BrowserRouter>
     <UserProvider>
       <App />
     </UserProvider>
-  </BrowserRouter>,
+  </BrowserRouter>
+  </ChakraProvider>,
   document.getElementById('root')
 );
 
