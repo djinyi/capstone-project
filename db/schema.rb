5dd238abdbfec8121e62c09194fe3f58287237fb
Daisy Yi <daisyyi@rocketmail.com>
2023-10-06 21:22:26 -0700

    remove password from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_21_170504) do
+ActiveRecord::Schema.define(version: 2023_10_07_041943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 2023_09_21_170504) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.string "name"
     t.string "family_members"
     t.string "address"
