11d24b1e0191d7c2749846a35074b849cef6d113
Daisy Yi <daisyyi@rocketmail.com>
2023-10-09 23:25:35 -0700

    change navbar look

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -17,26 +17,26 @@ function NavBar({ setLoggingIn }) {
 
 
     return (
-        <nav class="flex flex-row items-center justify-between flex-wrap bg-gray-100 p-6">
-            <ul class="float-left font-bold visited:bg-sky-600"><NavLink exact to="/">
+        <nav class="flex flex-row items-center justify-center flex-wrap bg-white p-3">
+            <ul class="float-left font-bold visited:bg-sky-600 mx-10"><NavLink exact to="/">
                 <p class="p-3 font-bold text-sky-500 hover:bg-sky-100 p-3">Home</p>
             </NavLink>
             </ul>
-            <ul class="float-left font-bold visited:bg-sky-600"><NavLink exact to="/profile">
+            <ul class="float-left font-bold visited:bg-sky-600 mx-10"><NavLink exact to="/profile">
                 <p class="p-3 text-sky-500 hover:bg-sky-100">Profile</p>
             </NavLink></ul>
-            <ul class="float-left font-bold visited:bg-sky-600"><NavLink exact to="/pets">
+            <ul class="float-left font-bold visited:bg-sky-600 mx-10"><NavLink exact to="/pets">
                 <p class="p-3 text-sky-500 hover:bg-sky-100">Pets</p>
             </NavLink></ul>
-            <ul class="float-left font-bold visited:bg-sky-600"><NavLink exact to="/contacts">
+            <ul class="float-left font-bold visited:bg-sky-600 mx-10"><NavLink exact to="/contacts">
                 <p class="p-3 text-sky-500 hover:bg-sky-100">Contacts</p>
             </NavLink>
             </ul>
-            <ul class="float-left font-bold visited:bg-sky-600"><NavLink exact to="/checklist">
+            <ul class="float-left font-bold visited:bg-sky-600 mx-10"><NavLink exact to="/checklist">
                 <p class="p-3 text-sky-500 hover:bg-sky-100">Checklist</p>
             </NavLink>
             </ul>
-            <ul class="float-left font-bold visited:bg-sky-600" onClick={handleLogoutClick}><NavLink exact to="/logout">
+            <ul class="float-left font-bold visited:bg-sky-600 mx-10" onClick={handleLogoutClick}><NavLink exact to="/logout">
                 <p class="p-3 text-sky-500 hover:bg-sky-100">Logout</p>
             </NavLink></ul>
         </nav>
