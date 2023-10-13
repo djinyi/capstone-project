4bad15c37ee7d84ede5c71037e7c207d38128e10
Daisy Yi <daisyyi@rocketmail.com>
2023-10-12 20:16:12 -0700

    add dependent destroy to checklist

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :pets, dependent: :destroy
     has_many :contacts, through: :pets, dependent: :destroy
-    has_many :checklist
+    has_many :checklist, dependent: :destroy
 
     has_secure_password
 
